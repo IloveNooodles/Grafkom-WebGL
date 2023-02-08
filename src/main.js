a0d0ec4745e26092bda289be4aaa15160c14bd05
IloveNooodles <mgarebaldhie80@gmail.com>
2023-02-08 21:32:01 +0700

    refactor : remove unnesecarry file:

--- src/main.js
+++ src/main.js
@@ -48,48 +48,28 @@ const canvas = document.getElementById("canvas");
 canvas.addEventListener("mousedown", function (e) {
   let { x, y } = getMousePosition(canvas, e);
   let { realWidth, realHeight } = transformCoordinate(canvas, x, y);
-  // console.log(realWidth, realHeight);
   draw();
 });
 
 /* ==== Global Object ==== */
-// const vertexShaderText = `
-//   precision mediump float;
-//   attribute vec2 vertPosition;
-//   attribute vec3 vertColor;
-//   varying vec3 fragColor;
-
-//   void main()
-//   {
-//     fragColor = vertColor;
-//     gl_Position = vec4(vertPosition, 0.0, 1.0);
-//   }`;
-
-// const fragmentShaderText = `
-//   precision mediump float;
-//   varying vec3 fragColor;
-//   void main(){
-//     gl_FragColor = vec4(fragColor, 1.0);
-//   }`;
-const vertexShaderText = `  // an attribute will receive data from a buffer
+const vertexShaderText = `
+  precision mediump float;  
   attribute vec4 a_position;
+  attribute vec3 vertColor;
+  varying vec3 fragColor;
 
-  // all shaders have a main function
   void main() {
-
-    // gl_Position is a special variable a vertex shader
-    // is responsible for setting
+    fragColor = vertColor;
     gl_Position = a_position;
   }`;
 
-const fragmentShaderText = ` // fragment shaders don't have a default precision so we need
-  // to pick one. mediump is a good default
+/* to check change frag color to rgb */
+const fragmentShaderText = `
   precision mediump float;
-
+  varying vec3 fragColor;
   void main() {
-    // gl_FragColor is a special variable a fragment shader
-    // is responsible for setting
-    gl_FragColor = vec4(1, 0, 0.5, 1); // return redish-purple
+    // gl_FragColor = vec4(fragColor, 1);
+    gl_FragColor = vec4(0.2,1,0.3, 1);
   }`;
 
 const gl = canvas.getContext("webgl");
@@ -102,7 +82,6 @@ window.onload = function start() {
     alert("WebGL not supported");
   }
 
-  /* setting viewport */
   clear();
 };
 
@@ -152,106 +131,46 @@ function createShaderProgram(vertexShaderText, fragmentShaderText) {
   }
 
   /* dont forget to delete after use it  */
-  // gl.deleteShader(vertexShader);
-  // gl.deleteShader(fragmentShader);
+  gl.deleteShader(vertexShader);
+  gl.deleteShader(fragmentShader);
 
   return program;
 }
 
 function draw(model) {
-  // create GLSL shaders, upload the GLSL source, compile the shaders
-  // Link the two shaders into a program
-  var program = createShaderProgram(vertexShaderText, fragmentShaderText);
+  if (!gl) {
+    alert("WebGL not supported");
+  }
+  const program = createShaderProgram(vertexShaderText, fragmentShaderText);
 
-  // look up where the vertex data needs to go.
-  var positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  if (!program) {
+    return;
+  }
 
-  // Create a buffer and put three 2d clip space points in it
-  var positionBuffer = gl.createBuffer();
-
-  // Bind it to ARRAY_BUFFER (think of it as ARRAY_BUFFER = positionBuffer)
+  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+  const positionBuffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
 
-  var positions = [0, 0, 0, 0.5, 0.7, 0];
+  var positions = [0, 0, 0, 0.5, 0.7, 0, 1, 0.2, 0.3];
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
 
-  // code above this line is initialization code.
-  // code below this line is rendering code.
-
-  resizeCanvas(gl.canvas);
-
-  // Tell WebGL how to convert from clip space to pixels
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+  clear();
 
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-
-  // Tell it to use our program (pair of shaders)
   gl.useProgram(program);
-
-  // Turn on the attribute
   gl.enableVertexAttribArray(positionAttributeLocation);
 
-  // Bind the position buffer.
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // Tell the attribute how to get data out of positionBuffer (ARRAY_BUFFER)
-  var size = 2; // 2 components per iteration
-  var type = gl.FLOAT; // the data is 32bit floats
-  var normalize = false; // don't normalize the data
-  var stride = 0; // 0 = move forward size * sizeof(type) each iteration to get the next position
-  var offset = 0; // start at the beginning of the buffer
+  /* transfer from arraybuffer to array in js */
   gl.vertexAttribPointer(
     positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    offset
+    2,
+    gl.FLOAT,
+    gl.FALSE,
+    0,
+    0
   );
-
-  // draw
-  var primitiveType = gl.TRIANGLES;
-  var offset = 0;
-  var count = 3;
-  gl.drawArrays(primitiveType, offset, count);
-  // if (!gl) {
-  //   alert("WebGL not supported");
-  // }
-
-  // clear();
-
-  // const program = createShaderProgram(vertexShaderText, fragmentShaderText);
-
-  // if (!program) {
-  //   return;
-  // }
-
-  // const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-  // const positionBuffer = gl.createBuffer();
-  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // var positions = [0, 0, 0, 0.5, 0.7, 0];
-  // gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  // gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-  // clear();
-
-  // gl.useProgram(program);
-  // gl.enableVertexAttribArray(positionAttributeLocation);
-
-  // gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  // /* transfer from arraybuffer to array in js */
-  // gl.vertexAttribPointer(
-  //   positionAttributeLocation,
-  //   2,
-  //   gl.FLOAT,
-  //   gl.FALSE,
-  //   0,
-  //   0
-  // );
-  // gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
 
   if (model == "line") {
     //line
@@ -264,15 +183,4 @@ function draw(model) {
   } else {
     return;
   }
-}
-
-function initBufferPosition() {
-  const bufferPos = gl.createBuffer();
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, bufferPos);
-  const positions = [1.0, 1.0, -1.0, 1.0, 1.0, -1.0, -1.0, -1.0];
-
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  return bufferPos;
 }
