704e55f93f3393375a9954a3022302d74a4deb69
Vionie Novencia Thanggestyo <13520006@std.stei.itb.ac.id>
2023-02-09 23:46:23 +0700

    feat: add clear button functionality to line

--- src/model.js
+++ src/model.js
@@ -1,20 +1,27 @@
 function line(canvas, gl, program){
-    let positions = [];
-    let colors = [];
-    canvas.addEventListener("mousedown", function (e) {
-      let { x, y } = getMousePosition(canvas, e);
-      let { realWidth, realHeight } = transformCoordinate(canvas, x, y);
-      positions.push(realWidth, realHeight);
-      colors.push(0, 0, 0, 1);
+  let positions = [];
+  let colors = [];
+  canvas.addEventListener("mousedown", function (e) {
+    let { x, y } = getMousePosition(canvas, e);
+    let { realWidth, realHeight } = transformCoordinate(canvas, x, y);
+    positions.push(realWidth, realHeight);
+    colors.push(0, 0, 0, 1);
 
-      if (positions.length % 4 == 0) {
-        renderColor(program, colors, 4);
-        renderVertex(program, positions, 2);
+    if (positions.length % 4 == 0) {
+      renderColor(program, colors, 4);
+      renderVertex(program, positions, 2);
 
-        clear();
-        for (var i = 0; i < positions.length; i += 2) {
-          gl.drawArrays(gl.LINES, i, 2);
-        }
+      clear();
+      for (var i = 0; i < positions.length; i += 2) {
+        gl.drawArrays(gl.LINES, i, 2);
       }
-    });
+    }
+  });
+
+  clearButton.addEventListener("click", function () {
+    clear();
+    positions = [];
+    colors = [];
+  }
+  );
 }
